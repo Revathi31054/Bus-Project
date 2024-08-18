b4f0459c2001c5a73a1f990938c15a259412172f
Revathi31054 <157214311+Revathi31054@users.noreply.github.com>
2024-08-18 21:34:10 +0530

    Create Bus.java

---
+++ Bus.java
@@ -0,0 +1,29 @@
+public class Bus {
+private int busno;
+private boolean ac;
+private int capacity;
+Bus(int no,boolean ac,int cap){
+	this.busno=no;
+	this.ac =ac;
+	this.capacity=cap;
+}
+
+public int getBusNo() {
+	return busno;
+}
+public boolean isAc() {
+	return ac;
+}
+public int getcapacity() {
+	return capacity;
+}
+public void setAc(boolean val) {
+	ac =val; 
+}
+public void setcapacity(int cap) {
+	capacity = cap; 
+}
+public void displayBusinfo(){
+	System.out.println("Bus No"+busno +"Ac:"+ac+ " Total Capacity:"+capacity);
+}
+}
